8551316b54463aaf5cd6980746cf2bdc34c7d6b2
HemantSharma9969 <61488985+HemantSharma9969@users.noreply.github.com>
2020-11-04 13:14:38 +0530

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
-import os
 from datetime import date
-import warnings
-warnings.filterwarnings("ignore")
+
 
 
 st.title("Lyricsstock.com analysis")
